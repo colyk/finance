d82a73dfff72077d54f26fcc7d05fa6cfe6fd138
oleksandrkobzar <kobzar.sasha1@gmail.com>
2019-12-14 20:16:58 +0100

    fix heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 require('express-async-errors');
 require('./routes')(app);
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
 
     const path = require('path');
@@ -12,6 +12,6 @@ require('./routes')(app);
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
 
-// }
+}
 
 module.exports = app;
