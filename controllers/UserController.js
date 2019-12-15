abddcd5f8eaba4f6d49c52032370808d7542976f
oleksandrkobzar <kobzar.sasha1@gmail.com>
2019-12-15 23:30:23 +0100

    Fix module sing-in for user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,19 +4,22 @@ const bcrypt = require('bcrypt');
 checkUser = (req, res) => {
     const body = req.body;
 
-    if (body) {
-        if (body.user.username !== '' && body.user.password !== '') {
-            User.findOne({ username: body.user.username }, (err, user) => {
-                if (err) return res.status(400).json({ success: false, error: err });
-                if (user !== null) {
-                    bcrypt.compare(body.user.password, user.password, function (err, result) {
-                        if (result) return res.status(200).json({ success: true, data: user });
-                        else return res.status(200).json({ success: false, error: "Invalid password" });
-                    });
-                } else return res.status(200).json({ success: false, error: "User not found" });
-            });
-        } else return res.status(200).json({ success: false, error: "Inputs are empty" });
-    } else return res.status(200).json({ success: false, error: "Error send data" });
+    if (!body)
+        return res.status(400).json({ error: "You must provide a user" });
+
+    if (!body.user.username && !body.user.password)
+        return res.status(200).json({ error: "Inputs are empty" });
+
+    User.findOne({ username: body.user.username }, (err, user) => {
+        if (err || !user)
+            return res.status(200).json({ error: "User not found" });
+
+        bcrypt.compare(body.user.password, user.password, function (err, result) {
+            if (result) 
+                return res.status(200).json({ data: user });
+            return res.status(200).json({ error: "Invalid password" });
+        });
+    });
 }
 
 module.exports = {
