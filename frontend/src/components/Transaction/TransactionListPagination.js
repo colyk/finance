4f2aca628e53d8bf4926ae58a7f191089a86b907
oleksandrkobzar <kobzar.sasha1@gmail.com>
2020-03-07 18:56:16 +0100

    Fix pagination

--- frontend/src/components/Transaction/TransactionListPagination.js
+++ frontend/src/components/Transaction/TransactionListPagination.js
@@ -1,47 +1,47 @@
 import React from 'react';
 
 const ListPagination = props => {
-
-  if (props.transactionsCount <= props.transactionsCountPerPage) {
-    return null;
-  }
+  if (props.transactionsCount <= props.transactionsCountPerPage) return null;
 
   const currentPage = parseInt(props.currentPage);
   const PageCount = Math.ceil(props.transactionsCount / props.transactionsCountPerPage);
 
   const range = [];
-  for (let i = 1; i <= PageCount; i++) {
-    range.push(i);
-  }
+  for (let i = 1; i <= PageCount; i++) range.push(i);
 
-  const onClickPage = (e, page) => {
-    e.preventDefault();
-    props.onSetPage(page, props.transactionsCountPerPage);
-  };
+  const onClickPage = page => props.onSetPage(page, props.transactionsCountPerPage);
 
   return (
     <nav>
       <ul className="pagination">
-        {currentPage === 1 ? null : (<li
-          className="page-item"
-          onClick={e => onClickPage(e, currentPage - 1)}>
-          <a className="page-link" href='/#'>Previous</a>
-        </li>)}
-        {
-          range.map((i) =>
-            <li
-              className={i === currentPage ? 'page-item active' : 'page-item'}
-              onClick={e => onClickPage(e, i)}
-              key={i.toString()}>
-              <a className="page-link" href='/#'>{i}</a>
-            </li>
-          )
-        }
-        {currentPage === PageCount ? null : (<li
-          className="page-item"
-          onClick={e => onClickPage(e, currentPage + 1)}>
-          <a className="page-link" href='/#'>Next</a>
-        </li>)}
+        <li className="page-item">
+          <button
+            className="btn"
+            onClick={() => onClickPage(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Previous
+          </button>
+        </li>
+        {range.map((i, index) => (
+          <li className="page-item" key={index}>
+            <button
+              className={currentPage === i ? 'btn btn-primary' : 'btn'}
+              onClick={() => onClickPage(i)}
+            >
+              {i}
+            </button>
+          </li>
+        ))}
+        <li className="page-item">
+          <button
+            className="btn"
+            onClick={() => onClickPage(currentPage + 1)}
+            disabled={currentPage === PageCount}
+          >
+            Next
+          </button>
+        </li>
       </ul>
     </nav>
   );
